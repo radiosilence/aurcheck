1d27a3e310a52f33fb79563b8441a77450bf525e
James Cleveland <jamescleveland@gmail.com>
2012-08-23 16:16:03 +0100

    using JSON APIs

--- aurcheck.py
+++ aurcheck.py
@@ -1,20 +1,30 @@
 #/usr/bin/env python3
 from time import sleep
+import json
 import smtplib
 import requests
+
+ARCH = 'x86_64'
+
+REPO_URL = 'https://www.archlinux.org/packages/{1}/{2}/{0}/json/'
+AUR_URL = 'https://aur.archlinux.org/rpc.php?type=info&arg={0}'
 
 packages = (
     (('dmenu', 'community'), 'dmenu-dogs'),
 )
 
 
-def pkgbuild_ver(pkgbuild):
-    lines = pkgbuild.decode('utf-8').split("\n")
-    ver = None
-    for line in lines:
-        if line.startswith('pkgver'):
-            _, ver = line.split('=')
-    return ver
+def get_ver(package, repo=None):
+    if repo:
+        return json.loads(requests.get(REPO_URL.format(
+            package,
+            repo,
+            ARCH
+        )).content.decode('utf-8'))['pkgver']
+    else:
+        return json.loads(requests.get(AUR_URL.format(
+            package
+        )).content.decode('utf-8'))['results']['Version'].split('-')[0]
 
 
 def ver_greater(a, b):
@@ -47,17 +57,11 @@ def send_email(msg):
 
     s.sendmail(sender, receivers, message)
 
-def check(pkg, aurpkg):
-    aur_url = 'https://aur.archlinux.org/packages/{}/{}/PKGBUILD'.format(
-        aurpkg[:2],
-        aurpkg
-    )
-    aur_ver = pkgbuild_ver(requests.get(aur_url).content)
-    pkg, repo = pkg
-    pkg_url = ('https://projects.archlinux.org/svntogit/{}.git/plain/trunk/PKG'
-        + 'BUILD?h=packages/{}').format(repo, pkg)
 
-    pkg_ver = pkgbuild_ver(requests.get(pkg_url).content)
+def check(pkg, aurpkg):
+    aur_ver = get_ver(aurpkg)
+    pkg, repo = pkg
+    pkg_ver = get_ver(pkg, repo=repo)
     if ver_greater(pkg_ver, aur_ver):
         return True, '{} version ({}) is > AUR {} version ({})'.format(
             pkg,
